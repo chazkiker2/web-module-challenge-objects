12f73da791a926562bbaa7c78aeb548b473588f8
Chaz Kiker <chazkiker@gmail.com>
2020-09-10 04:45:27 -0400

    finish task 4: add new rating (p.s. last commit was supposed to say finish task 3)

--- index.js
+++ index.js
@@ -85,10 +85,12 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 ]
 
 /* Task 3: Console.log just Julius' feedback */
-console.log(reviews[5].feedback);
+// console.log(reviews[5].feedback);
 
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+reviews.push({name: "Chaz", rating: 4, feedback: "great food if someone else is paying!"});
+// console.log(reviews[reviews.length-1]);
 
 
 /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
